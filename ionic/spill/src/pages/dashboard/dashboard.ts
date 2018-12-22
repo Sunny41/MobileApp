d6b073400732f899296319c63f1676a2d57c24de
Cthulhu <jannik.renner@Student.reutlingen-university.de>
2018-12-22 18:28:42 +0100

    dashboard layout

--- ionic/spill/src/pages/dashboard/dashboard.ts
+++ ionic/spill/src/pages/dashboard/dashboard.ts
@@ -10,14 +10,34 @@ import { GroupPage } from '../group/group';
 })
 export class DashboardPage {
 
+  groupList = [];
   GROUPS =  [
     {id:1, name:"Shooters", description:"Meeting at shooter Rt at 20:00"},
     {id:2, name:"Cinema", description:"With Mike and Ellie"},
-    {id:3, name:"Trip to Berlin", description:"Don't forget to create activities"},
+    {id:3, name:"Trip to Berlin", description:"Don't forget to create activities"}
   ];
+
+  notifications = " 1";
 
   constructor(public navCtrl: NavController) {
     this.navCtrl = navCtrl;
+
+    for (let i = 0; i < this.GROUPS.length; i++) {
+      this.groupList.push( this.GROUPS[i] );
+    }
+  }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < this.GROUPS.length; i++) {
+        this.groupList.push( this.GROUPS[i] );
+      }
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
   }
 
   openSettings(){
