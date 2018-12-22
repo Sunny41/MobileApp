c5a1369dd42716b6f324dcf607fc6afa15a2d8ce
Oliver Wagner <oliver.wagner@student.reutlingen-university.de>
2018-12-22 23:17:22 +0100

    API for fetching data from the database / small changes to db structure
    also added example data for all tables

--- mobile_db.sql
+++ mobile_db.sql
@@ -27,9 +27,15 @@ CREATE TABLE IF NOT EXISTS `Activity` (
   PRIMARY KEY (`activityId`),
   KEY `activityAdminId` (`activityAdminId`),
   CONSTRAINT `activityAdminId` FOREIGN KEY (`activityAdminId`) REFERENCES `User` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table mobileapp.Activity: ~1 rows (approximately)
+DELETE FROM `Activity`;
+/*!40000 ALTER TABLE `Activity` DISABLE KEYS */;
+INSERT INTO `Activity` (`activityId`, `name`, `description`, `date`, `place`, `activityAdminId`) VALUES
+	(2, 'Clubnight', 'Party hard!', '2018-12-31 23:00:00', 'FlyHighClub', 2);
+/*!40000 ALTER TABLE `Activity` ENABLE KEYS */;
+
 -- Dumping structure for table mobileapp.ActivityMembers
 CREATE TABLE IF NOT EXISTS `ActivityMembers` (
   `activityMembersActivityId` int(11) DEFAULT NULL COMMENT 'ID of the activity',
@@ -40,7 +46,14 @@ CREATE TABLE IF NOT EXISTS `ActivityMembers` (
   CONSTRAINT `activityMembersUserId` FOREIGN KEY (`activityMembersUserId`) REFERENCES `User` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This table contains all the members participating in an activity';
 
--- Data exporting was unselected.
+-- Dumping data for table mobileapp.ActivityMembers: ~2 rows (approximately)
+DELETE FROM `ActivityMembers`;
+/*!40000 ALTER TABLE `ActivityMembers` DISABLE KEYS */;
+INSERT INTO `ActivityMembers` (`activityMembersActivityId`, `activityMembersUserId`) VALUES
+	(2, 1),
+	(2, 2);
+/*!40000 ALTER TABLE `ActivityMembers` ENABLE KEYS */;
+
 -- Dumping structure for table mobileapp.Group
 CREATE TABLE IF NOT EXISTS `Group` (
   `groupId` int(11) NOT NULL AUTO_INCREMENT,
@@ -50,9 +63,15 @@ CREATE TABLE IF NOT EXISTS `Group` (
   PRIMARY KEY (`groupId`),
   KEY `groupAdminId` (`groupAdminId`),
   CONSTRAINT `groupAdminId` FOREIGN KEY (`groupAdminId`) REFERENCES `User` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table mobileapp.Group: ~1 rows (approximately)
+DELETE FROM `Group`;
+/*!40000 ALTER TABLE `Group` DISABLE KEYS */;
+INSERT INTO `Group` (`groupId`, `name`, `description`, `groupAdminId`) VALUES
+	(1, 'Berlin Trip', 'Our trip to Berlin', 1);
+/*!40000 ALTER TABLE `Group` ENABLE KEYS */;
+
 -- Dumping structure for table mobileapp.GroupActivity
 CREATE TABLE IF NOT EXISTS `GroupActivity` (
   `groupId` int(11) DEFAULT NULL,
@@ -63,7 +82,13 @@ CREATE TABLE IF NOT EXISTS `GroupActivity` (
   CONSTRAINT `GroupActivityGroupId` FOREIGN KEY (`groupId`) REFERENCES `Group` (`groupId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This table contains the activites of a group';
 
--- Data exporting was unselected.
+-- Dumping data for table mobileapp.GroupActivity: ~1 rows (approximately)
+DELETE FROM `GroupActivity`;
+/*!40000 ALTER TABLE `GroupActivity` DISABLE KEYS */;
+INSERT INTO `GroupActivity` (`groupId`, `activityId`) VALUES
+	(1, 2);
+/*!40000 ALTER TABLE `GroupActivity` ENABLE KEYS */;
+
 -- Dumping structure for table mobileapp.GroupMembers
 CREATE TABLE IF NOT EXISTS `GroupMembers` (
   `groupId` int(11) DEFAULT NULL,
@@ -74,22 +99,62 @@ CREATE TABLE IF NOT EXISTS `GroupMembers` (
   CONSTRAINT `groupMembersUserId` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This table contains all the members of a group';
 
--- Data exporting was unselected.
+-- Dumping data for table mobileapp.GroupMembers: ~2 rows (approximately)
+DELETE FROM `GroupMembers`;
+/*!40000 ALTER TABLE `GroupMembers` DISABLE KEYS */;
+INSERT INTO `GroupMembers` (`groupId`, `userId`) VALUES
+	(1, 1),
+	(1, 2);
+/*!40000 ALTER TABLE `GroupMembers` ENABLE KEYS */;
+
+-- Dumping structure for table mobileapp.Invitation
+CREATE TABLE IF NOT EXISTS `Invitation` (
+  `invitationId` int(11) NOT NULL AUTO_INCREMENT,
+  `invitedUserId` int(11) NOT NULL DEFAULT '0' COMMENT 'ID of the user who is invited',
+  `invitationFromUserId` int(11) NOT NULL DEFAULT '0' COMMENT 'ID of the user who sent the invitation',
+  `invitationForGroupId` int(11) NOT NULL DEFAULT '0' COMMENT 'ID of the group the invitation was sent for',
+  PRIMARY KEY (`invitationId`),
+  KEY `invitedUserId` (`invitedUserId`),
+  KEY `invitationFromUserId` (`invitationFromUserId`),
+  KEY `invitationForGroupId` (`invitationForGroupId`),
+  CONSTRAINT `invitationForGroupId` FOREIGN KEY (`invitationForGroupId`) REFERENCES `Group` (`groupId`),
+  CONSTRAINT `invitationFromUserId` FOREIGN KEY (`invitationFromUserId`) REFERENCES `User` (`userId`),
+  CONSTRAINT `invitedUserId` FOREIGN KEY (`invitedUserId`) REFERENCES `User` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table mobileapp.Invitation: ~0 rows (approximately)
+DELETE FROM `Invitation`;
+/*!40000 ALTER TABLE `Invitation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Invitation` ENABLE KEYS */;
+
 -- Dumping structure for table mobileapp.Item
 CREATE TABLE IF NOT EXISTS `Item` (
   `itemId` int(11) NOT NULL AUTO_INCREMENT,
+  `itemName` varchar(50) NOT NULL,
+  `itemDescription` varchar(50) DEFAULT NULL COMMENT 'Further description for an item if necessary',
   `itemUserId` int(11) NOT NULL DEFAULT '0',
   `amount` double NOT NULL DEFAULT '0',
   `itemActivityId` int(11) NOT NULL DEFAULT '0' COMMENT 'ID of the activity the item belongs to',
   PRIMARY KEY (`itemId`),
   KEY `itemActivityId` (`itemActivityId`),
-  CONSTRAINT `itemActivityId` FOREIGN KEY (`itemActivityId`) REFERENCES `Activity` (`activityId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This table will be used for normal queries - for invitations use itemInvited';
+  KEY `itemUserId` (`itemUserId`),
+  CONSTRAINT `itemActivityId` FOREIGN KEY (`itemActivityId`) REFERENCES `Activity` (`activityId`),
+  CONSTRAINT `itemUserId` FOREIGN KEY (`itemUserId`) REFERENCES `User` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1 COMMENT='This table will be used for normal queries - for invitations use itemInvited';
 
--- Data exporting was unselected.
--- Dumping structure for table mobileapp.itemInvited
-CREATE TABLE IF NOT EXISTS `itemInvited` (
+-- Dumping data for table mobileapp.Item: ~2 rows (approximately)
+DELETE FROM `Item`;
+/*!40000 ALTER TABLE `Item` DISABLE KEYS */;
+INSERT INTO `Item` (`itemId`, `itemName`, `itemDescription`, `itemUserId`, `amount`, `itemActivityId`) VALUES
+	(5, 'Fanta', NULL, 1, 2, 2),
+	(8, 'Pizza Quattro Formaggi', NULL, 2, 6.5, 2);
+/*!40000 ALTER TABLE `Item` ENABLE KEYS */;
+
+-- Dumping structure for table mobileapp.ItemInvited
+CREATE TABLE IF NOT EXISTS `ItemInvited` (
   `itemInvitedId` int(11) NOT NULL AUTO_INCREMENT,
+  `itemName` varchar(50) NOT NULL DEFAULT '0',
+  `itemDescription` varchar(50) NOT NULL DEFAULT '0',
   `amount` double NOT NULL DEFAULT '0',
   `itemInviteActivityId` int(11) NOT NULL DEFAULT '0' COMMENT 'ID of the activity the item belongs to',
   `itemInviteUserId` int(11) NOT NULL DEFAULT '0' COMMENT 'the user who''s inviting another person',
@@ -101,9 +166,15 @@ CREATE TABLE IF NOT EXISTS `itemInvited` (
   CONSTRAINT `itemInviteActivityId` FOREIGN KEY (`itemInviteActivityId`) REFERENCES `Activity` (`activityId`),
   CONSTRAINT `itemInviteInvitedUserId` FOREIGN KEY (`itemInviteInvitedUserId`) REFERENCES `User` (`userId`),
   CONSTRAINT `itemInviteUserId` FOREIGN KEY (`itemInviteUserId`) REFERENCES `User` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='If someone takes an item for another person, this table will be used for queries';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='If someone takes an item for another person, this table will be used for queries';
 
--- Data exporting was unselected.
+-- Dumping data for table mobileapp.ItemInvited: ~1 rows (approximately)
+DELETE FROM `ItemInvited`;
+/*!40000 ALTER TABLE `ItemInvited` DISABLE KEYS */;
+INSERT INTO `ItemInvited` (`itemInvitedId`, `itemName`, `itemDescription`, `amount`, `itemInviteActivityId`, `itemInviteUserId`, `itemInviteInvitedUserId`) VALUES
+	(2, 'Pizza Funghi', '0', 7.5, 2, 2, 1);
+/*!40000 ALTER TABLE `ItemInvited` ENABLE KEYS */;
+
 -- Dumping structure for table mobileapp.User
 CREATE TABLE IF NOT EXISTS `User` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
@@ -111,9 +182,16 @@ CREATE TABLE IF NOT EXISTS `User` (
   `username` varchar(50) NOT NULL DEFAULT '0' COMMENT 'display name',
   `password` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table mobileapp.User: ~2 rows (approximately)
+DELETE FROM `User`;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` (`userId`, `mail`, `username`, `password`) VALUES
+	(1, 'oliver.wagner@student.reutlingen-university.de', 'Oli', 'Hallo'),
+	(2, 'peter.vogel@funfunfun.com', 'Peter', 'Pwd1212!');
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ spill_api/app.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mysql= require('mysql');
+var http = require('http');
+
+
+var index = require('./routes/index');
+var activities = require('./routes/activities');
+var activitymembers = require('./routes/activitymembers');
+var groups = require('./routes/groups');
+var groupactivities = require('./routes/groupactivities');
+var groupmembers = require('./routes/groupmembers');
+var invitations = require('./routes/invitations');
+var items = require('./routes/items');
+var itemsinvited = require('./routes/itemsinvited');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//Database connection
+app.use(function(req, res, next){
+	global.connection = mysql.createConnection({
+	  	host     : 'cloudws1819.c0lwjxnry6gy.us-east-2.rds.amazonaws.com',
+	  	user     : 'mobile',
+        password : 'Mobile1234!',
+  		database : 'mobileapp'
+	});
+	connection.connect();
+	next();
+});
+
+//API GET Calls
+app.use('/', index);
+app.use('/activities', activities); //GET all Activities
+app.use('/activitiymembers', activitymembers); //GET all members of an activity
+app.use('/groups', groups); //GET all groups
+app.use('/groupactivities', groupactivities); //GET all activities of a group
+app.use('/groupmembers', groupmembers); //GET all members of a group
+app.use('/invitations', invitations); //GET all invitations
+app.use('/items', items); //GET all items
+app.use('/itemsinvited', itemsinvited); //GET all items someone has been invited for
+app.use('/users', users); //GET all users
+
+
+//API POST Calls
+/*...*/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+var server = http.createServer(app);
+server.listen(3000);
