23df6a09e69fc78e336db420eeeed88b4b489082
Oliver Wagner <oliver.wagner@student.reutlingen-university.de>
2019-01-04 18:00:41 +0100

    api: more additional request options

--- spill_api/routes/activities.js
+++ spill_api/routes/activities.js
@@ -14,6 +14,19 @@ router.get('/', function(req, res, next) {
   	});
 });
 
+/* GET activities listing, search query for id. */
+router.get('/id', function(req, res, next) {
+    connection.query('SELECT * from Activity WHERE activityId = ?',req.query.s, function (error, results, fields) {
+        if(error){
+            res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+            //If there is error, we send the error in the error section with 500 status
+        } else {
+            res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+            //If there is no error, all is good and response is 200OK.
+        }
+    });
+});
+
 /* GET activities listing, search query for name. */
 router.get('/name', function(req, res, next) {
 	connection.query('SELECT * from Activity WHERE name like  ?','%'+req.query.s+'%', function (error, results, fields) {
--- spill_api/routes/groups.js
+++ spill_api/routes/groups.js
@@ -14,6 +14,19 @@ router.get('/', function(req, res, next) {
   	});
 });
 
+/* GET groups listing, search query for id. */
+router.get('/id', function(req, res, next) {
+	connection.query('SELECT * from mobileapp.Group WHERE groupId = ? ',req.query.s, function (error, results, fields) {
+		if(error){
+			res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+			//If there is error, we send the error in the error section with 500 status
+		} else {
+			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+			//If there is no error, all is good and response is 200OK.
+		}
+	});
+});
+
 /* GET groups listing, search query for name. */
 router.get('/name', function(req, res, next) {
 	connection.query('SELECT * from mobileapp.Group WHERE name like ? ','%'+req.query.s+'%', function (error, results, fields) {
