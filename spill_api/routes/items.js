228c7806613f477acea69b703f5776c4a0de35c9
Oliver Wagner <oliver.wagner@student.reutlingen-university.de>
2019-01-17 16:11:19 +0100

    api: implemented delete calls for users, activities, groups, invitations, items and itemsinvited

--- spill_api/routes/items.js
+++ spill_api/routes/items.js
@@ -125,4 +125,18 @@ router.put('/edit', function (req,res,next) {
 	});
 });
 
+
+/* delete item by id*/
+router.delete('/delete', function(req, res, next) {
+	connection.query('DELETE from Item WHERE itemId = ?', req.query.id, function (error, results, fields) {
+		if(error){
+			res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+			//If there is error, we send the error in the error section with 500 status
+		} else {
+			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+			//If there is no error, all is good and response is 200OK.
+		}
+	});
+});
+
 module.exports = router;
