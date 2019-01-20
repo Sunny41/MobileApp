9028a8640545244ff06dc54fb3d2c14e46ff045d
Cthulhu <jannik.renner@Student.reutlingen-university.de>
2019-01-20 17:42:03 +0100

    corrected. waiting for new api

--- ionic/spill/src/pages/new-item/new-item.ts
+++ ionic/spill/src/pages/new-item/new-item.ts
@@ -2,14 +2,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ActivityPage } from '../activity/activity';
-//import { HttpClient } from '@angular/common/http';
 import { HTTP } from '@ionic-native/http';
-/**
- * Generated class for the NewPostPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-new-item',
@@ -23,10 +16,6 @@ export class NewItemPage {
   itemName:string;
   itemDescription:string;
   cost:number;
-
-  name: String;
-  description: string;
-  costs: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: HTTP) {
     this.group = navParams.get('group');
@@ -51,40 +40,37 @@ export class NewItemPage {
 
   addNewAsset() {
     //store in db:
-    // in items table:
-    // {itemName, itemDescription, itemUserId, amount, itemActivityId}
-    var url = 'https://spillapi.mybluemix.net/items/new?itemName=' + this.name + '&itemDescription=' + this.description
-    + '&itemUserId=' + this.user.userId + '&amount=' + this.costs + '&itemActivityId=' + this.activity.activityId ;
-    this.http.post(url, {}, {}).then(data=>{
-      var result: any = JSON.parse(data.data);
-      console.log("status " + data.status);
-      if(data.status == 200){
-        alert(1)
-        console.log("data " + data.data);
-        var item: any = result.response[0];
-        this.navCtrl.push(ActivityPage, { activity: this.activity, user: this.user, group: this.group, item: item });
+   // for each member that is checked
+    var json = {"userId":this.user.userId, "username":this.user.username, "checked":true};
+    this.activityMembers.push(json);
+    
+    var checkedUser;
+    for(var i=0; i<this.activityMembers.length; i++){
+      	if(this.activityMembers[i].checked){
+          checkedUser++;
+        }
+    }
 
-    // for each member one entry in itemsInvited (checkedMembers):
-    // {itemName, itemDescription, itemUserId, amount, itemInviteActivityId, itemInviteUserId, itemInviteInvitedUserid}
-    for (var i = 0; i <= this.checkedMembers.length-1; i += 1) {
-      if(this.checkedMembers[i]== true){
-        var url = 'https://spillapi.mybluemix.net/itemsinvited/new?itemName=' + this.name + '&itemDescription=' + this.description
-        + '&itemUserId=' + this.user.userId + '&amount=' + this.costs
+    var amount = this.cost / checkedUser;
+
+    //Change API function so that we only need one call
+    for (var i = 0; i <= this.activityMembers.length-1; i += 1) {
+      if(this.activityMembers[i].checked){
+        var url = 'https://spillapi.mybluemix.net/itemsinvited/new?itemName=' + this.itemName + '&itemDescription=' + this.itemDescription
+        + '&itemUserId=' + this.user.userId + '&amount=' + amount
         + '&itemInviteActivityId=' + this.activity.activityId + '&itemInviteUserId=' + this.user.userId + '&itemInviteInvitedUserid=' + this.activityMembers[i].userId;
         this.http.post(url, {},{}).then(data=>{
           var result: any = JSON.parse(data.data);
-          console.log("status " + data.status);
           if(data.status == 200){
-            console.log("data " + data.data);
+            
           }else{
             alert("something went wrong with adding members to the Acticity. please try again!")
           }
         });
+        }
       }
 
     }
-
-  }
-});
-}
+      
+    
 }
