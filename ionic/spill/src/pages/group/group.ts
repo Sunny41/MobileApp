fe6d83110a249073ac2fcf350df67472cc81622e
Hanna Schulze <Hanna_Frederike.Schulze@student.reutlingen-university.de>
2019-01-21 15:22:12 +0100

    enter Activity if not already joined. done

--- ionic/spill/src/pages/group/group.ts
+++ ionic/spill/src/pages/group/group.ts
@@ -1,7 +1,7 @@
 //Author: Sonja Czernotzky, Hanna Schulze
 
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { NewActivityPage } from '../new-activity/new-activity';
 import { ActivityPage } from '../activity/activity';
 import { AddMemberPage } from '../add-member/add-member';
@@ -22,8 +22,7 @@ export class GroupPage {
   groupMembers: any = [];
   groupMembersID: any = [];
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HTTP) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HTTP, public alertCtrl: AlertController) {
     if (navParams.get('group') != null) {
       //get ids from navparams
       this.group = navParams.get('group');
@@ -46,18 +45,6 @@ export class GroupPage {
                 for (var j = 0; j < result.response.length; j++) {
                   this.activities.push(result.response[j]);
                 }
-                  //get activitymembers over activityid
-                  var url = 'https://spillapi.mybluemix.net/activitymembers/id?s=' + this.groupactivities[i].activityId;
-                  this.http.get(url, {}, {}).then(data => {
-                    var result: any = JSON.parse(data.data);
-                    if(data.status == 200){
-                      for ( var k = 0; k < result.response.length; k++) {
-                        if(this.user.userId == result.response[k].userId){
-                          
-                        }
-                      }
-                    }
-                  });
               }
             });
           }
@@ -97,18 +84,58 @@ export class GroupPage {
   }
 
   joinActivity(activity: any){
-    //add Members to the Activity if not already in
-    
-      var url = 'https://spillapi.mybluemix.net/activitymembers/new?activityId='+activity.activityId+'&userId='+this.user.userId;
-
-      
-      console.log("ActivityId:" + activity.activityId + ", UserId:" + this.user.userId);
-
-      this.http.post(url, { activityId: activity.activityId, userId: this.user.userId },{});
-
-      this.openActivity(activity);
-    
-  }
+    //add Members to the Activity if not already in, show Pop Up if not already in 
+    //get activitymembers over activityid
+    var alreadyjoined: boolean = false;
+    var url = 'https://spillapi.mybluemix.net/activitymembers/id?s=' + activity.activityId;
+    this.http.get(url, {}, {}).then(data => {
+      var result: any = JSON.parse(data.data);
+      if(data.status == 200){
+        console.log(result.response);
+        for ( var k = 0; k < result.response.length; k++) {
+          
+          //console.log("User: " + result.response[k].activityMembersUserId);
+          
+          if(this.user.userId == result.response[k].activityMembersUserId) {
+            //console.log("User: " + result.response[k].activityMembersUserId);
+            alreadyjoined = true;
+          }
+        }
+        if(!alreadyjoined){
+          const join = this.alertCtrl.create({
+            title: 'Join Activity?',
+            message: 'Do you want to join the Activity?',
+            buttons: [
+              {
+                text: 'Cancel',
+                role: 'cancel',
+                handler: () => {
+                  //console.log('Cancel clicked');
+                }
+              },
+              {
+                text: 'Join',
+                handler: () => {
+                  var url = 'https://spillapi.mybluemix.net/activitymembers/new?activityId=' + activity.activityId + '&userId=' + this.user.userId;
+                  this.http.post(url, {},{}).then(data=>{
+                    var result: any = JSON.parse(data.data);
+                    if(data.status == 200){
+                      console.log('User Added');
+                    }
+                  });
+                  console.log('Join clicked');
+                }
+              }
+            ]
+          });
+          join.present();
+        }else{
+          this.openActivity(activity);
+        }
+      }
+    });
+  }   
+  
 
   openAddMember() {
     this.navCtrl.push(AddMemberPage, {user: this.user, group: this.group});
